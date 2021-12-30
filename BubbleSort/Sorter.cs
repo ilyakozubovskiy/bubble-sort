8b24f7b4f08529495b2cd24fd5918e9cd45e33ee
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-12-30 11:55:13 +0300

    Bubble Sort Algorithm

--- BubbleSort/Sorter.cs
+++ BubbleSort/Sorter.cs
@@ -12,8 +12,23 @@ namespace BubbleSort
         /// </summary>
         public static void BubbleSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -21,8 +36,32 @@ namespace BubbleSort
         /// </summary>
         public static void RecursiveBubbleSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            RecursiveBubbleSort(array, 0);
+        }
+
+        public static void RecursiveBubbleSort(int[] array, int startIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0 || startIndex == array.Length - 1)
+            {
+                return;
+            }
+
+            for (int j = 0; j < array.Length - startIndex - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+
+            RecursiveBubbleSort(array, startIndex + 1);
         }
     }
 }
